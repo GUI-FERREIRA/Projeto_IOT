c2a7607d080ee15b07fcf87fad1f0e0cc2dd9059
AnaJúliaCS <anajulia.costasantana@gmail.com>
2021-05-01 17:37:56 -0300

    Atualizaçao na Comunicaçao Serial

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -57,7 +57,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/gui/Desktop/Semana07/web/env'
+set -gx VIRTUAL_ENV '/home/anacs/Projeto_IOT/env'
 
 # https://github.com/fish-shell/fish-shell/issues/436 altered PATH handling
 if test (echo $FISH_VERSION | head -c 1) -lt 3
--- env/bin/activate.xsh
+++ env/bin/activate.xsh
@@ -28,7 +28,7 @@ def _deactivate(args):
 _deactivate(["nondestructive"])
 aliases["deactivate"] = _deactivate
 
-$VIRTUAL_ENV = r"/home/gui/Desktop/Semana07/web/env"
+$VIRTUAL_ENV = r"/home/anacs/Projeto_IOT/env"
 
 $_OLD_VIRTUAL_PATH = $PATH
 $PATH = $PATH[:]
